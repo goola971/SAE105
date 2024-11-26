46323766dc321a4e69237cc8f56c2a4ddb760123
goola971 <janviercharly97114@gmail.com>
2024-11-26 12:35:21 +0100

    save

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	var menu = document.getElementById("header_menu");
 	var menu_close = document.getElementById("burger_close");
 	var menu_open = document.getElementById("burger_open");
-
+	var search = document.getElementById("search");
+	var button_search = document.getElementById("button_search");
+	var input_search = document.getElementById("input_search");
 	menu.style.display = "none";
 	menu_close.style.display = "none";
+	input_search.style.display = "none";
+	search.style.backgroundColor = "transparent";
+	button_search.style.filter = "invert(0)";
 	cards.forEach((card) => {
 		const desc = card.querySelector(".desc");
 		const descp = desc.querySelector("p");
@@ -37,6 +42,17 @@ document.addEventListener("DOMContentLoaded", () => {
 			menu.style.display = "none";
 			menu_close.style.display = "none";
 			menu_open.style.display = "block";
+		}
+	});
+	button_search.addEventListener("click", () => {
+		if (input_search.style.display === "none") {
+			input_search.style.display = "block";
+			search.style.backgroundColor = "white";
+			button_search.style.filter = "invert(1)";
+		} else {
+			input_search.style.display = "none";
+			search.style.backgroundColor = "transparent";
+			button_search.style.filter = "invert(0)";
 		}
 	});
 });
