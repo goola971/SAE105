d09791d060108afea1a3a61f9c8b5e41ef261bac
goola971 <janviercharly97114@gmail.com>
2024-11-26 12:03:42 +0100

    save

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
 	const buttontotop = document.getElementById("button_top");
 	var button_menu = document.getElementById("button_menu");
 	var menu = document.getElementById("header_menu");
-	var menu_close = document.getElementById("menu_close");
-	var menu_open = document.getElementById("menu_open");
+	var menu_close = document.getElementById("burger_close");
+	var menu_open = document.getElementById("burger_open");
 
 	menu.style.display = "none";
+	menu_close.style.display = "none";
 	cards.forEach((card) => {
 		const desc = card.querySelector(".desc");
 		const descp = desc.querySelector("p");
@@ -30,8 +31,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	button_menu.addEventListener("click", () => {
 		if (menu.style.display === "none") {
 			menu.style.display = "flex";
+			menu_close.style.display = "block";
+			menu_open.style.display = "none";
 		} else {
 			menu.style.display = "none";
+			menu_close.style.display = "none";
+			menu_open.style.display = "block";
 		}
 	});
 });
